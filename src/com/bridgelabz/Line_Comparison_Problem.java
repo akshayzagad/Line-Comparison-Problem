a7e8ecffb27ea5b7289b90a7449723caefc6437c
akshayzagad <akshayzagade62@gmail.com.com>
2022-10-05 16:09:52 +0530

    Added: Two geometry Cordinates length

--- src/com/bridgelabz/Line_Comparison_Problem.java
+++ src/com/bridgelabz/Line_Comparison_Problem.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Line_Comparison_Problem {
     public static void main(String[] args) {
-        System.out.println("Welcome To Line Comparison Problem");
+        int x1, y1, x2, y2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2");
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
+        lineLenghtCal(x1,y1,x2,y2);
+
+    }
+    static void lineLenghtCal(int x1,int x2,int y1,int y2){
+    int lineLength = (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    System.out.println("The length of line is: "+lineLength);
     }
 }
