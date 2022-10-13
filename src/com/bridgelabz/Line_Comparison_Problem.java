95cfa1062fb14d803d76d0a11a49521a872394aa
akshayzagad <akshayzagade62@gmail.com.com>
2022-10-13 21:48:40 +0530

    Added: Two geometry Cordinates length Are Equal Using Compare To Method

--- src/com/bridgelabz/Line_Comparison_Problem.java
+++ src/com/bridgelabz/Line_Comparison_Problem.java
@@ -19,24 +19,14 @@ public class Line_Comparison_Problem {
         b2 = scanner.nextInt();
        int lineOneLength= lineLenghtCal(x1,y1,x2,y2);
         int lineSecondLength= lineLenghtCal(a1,b1,a2,b2);
-        System.out.println("The length of line 1 is: "+lineOneLength);
-        System.out.println("The length of line 2 is: "+lineSecondLength);
-        equalityCheck(lineOneLength,lineSecondLength);
+        Integer lineOneLength1=new Integer(lineOneLength);
+        Integer lineOneLength2=new Integer(lineSecondLength);
+        System.out.println( lineOneLength1.equals(lineOneLength2));
+//        System.out.println( lineOneLength.equals(lineOneLength));
     }
     static int lineLenghtCal(int x1, int x2, int y1, int y2){
     int lineLength = (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     System.out.println("The length of line is: "+lineLength);
         return lineLength;
-    }
-    static void equalityCheck(int lineOneLength , int lineTwoLength ) {
-        if(lineOneLength == lineTwoLength ) {
-            System.out.println("Lines are Equal");
-        }
-        else if(lineOneLength > lineTwoLength){
-            System.out.println("Line 1 is greater than Line 2 ");
-        }
-        else {
-            System.out.println("Line 2 is greater than Line 1 ");
-        }
     }
 }
